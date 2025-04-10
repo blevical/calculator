37966a7f65fb245d05f31a0efee3d90bfa3fd7c4
Caleb Blevins <blevsca2@gmail.com>
2025-04-10 14:59:32 -0400

    manipulated the display in js to show values for numbers and operators.

--- script.js
+++ script.js
@@ -11,15 +11,38 @@ const multiply = function(a,b) {
 };
 
 const divide = function(a,b) {
-    return a % b;
+    if (b === 0){
+        return "Error";
+    }
+    return a / b
 };
 
-const newDiv = document.getElementById("display")
+const displayElement = document.querySelector(".display");
+const numberButtons = document.querySelectorAll(".number");
+const operatorButtons = document.querySelectorAll(".operator");
+const clearButton = document.querySelector(".clear");
 
-const button = document.getElementsByClassName("buttons")
+let firstOperand = null;
+let secondOperand = null;
+let currentOperator = null;
 
-const operations = [add, subtract, multiply, divide]
+function userDisplay(value) {
+    displayElement.textContent = value;
+}
 
-function operate(a,b) {
-    
+function operate(operator, a , b) {
+    a = Number(a);
+    b = Number(b);
+    switch (operator) {
+        case "+":
+            return add(a,b);
+        case "-":
+            return subtract(a,b);
+        case "*":
+            return multiply(a,b);
+        case "/":
+            return divide(a,b);
+        default:
+            return null;
+    }
 }
